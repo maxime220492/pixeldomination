0a749a35a18c16635bb3ac7727fcb928cff169b1
maxime220492 <57636465+maxime220492@users.noreply.github.com>
2024-05-26 07:18:23 +1000

    Delete functions directory

--- functions/getClans.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-    // Assuming you have a function to get all clans from a database
-    const clans = await getAllClansFromDatabase();
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(clans),
-    };
-};
